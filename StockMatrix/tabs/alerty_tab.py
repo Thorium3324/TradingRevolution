0c1c2529897988a561f3378f142be6d022056a0f
Thorium3324 <118537377+Thorium3324@users.noreply.github.com>
2025-10-27 05:08:32 +0100

    Update alerty_tab.py

--- StockMatrix/tabs/alerty_tab.py
+++ StockMatrix/tabs/alerty_tab.py
@@ -2,9 +2,6 @@ import streamlit as st
 from utils.alerts import check_alerts
 
 def alerty_tab():
-    st.title("🚨 Alerty")
-    symbol = st.text_input("Symbol:", "AAPL")
-    if st.button("Sprawdź alerty"):
-        alerts = check_alerts(symbol)
-        for a in alerts:
-            st.warning(a)
+    st.title("🔔 Alerty")
+    msg = check_alerts()
+    st.success(msg)
