67dbbf5ee85ec7f066db56add34c2bd7d96bdd81
Thorium3324 <118537377+Thorium3324@users.noreply.github.com>
2025-10-27 03:52:11 +0100

    heatmap.py

--- StockMatrix/utils/heatmap.py
+++ StockMatrix/utils/heatmap.py
@@ -1,10 +1,11 @@
 import plotly.graph_objects as go
+import pandas as pd
 
 def plot_prediction_heatmap(df_actual, df_pred):
-    df_pct = ((df_pred - df_actual)/df_actual).T * 100
+    df_pct = ((df_pred - df_actual)/df_actual)*100
     fig = go.Figure()
     for col in df_pct.columns:
-        fig.add_trace(go.Heatmap(z=df_pct[col].values.reshape(1,-1), x=df_pct.index, y=[col]*len(df_pct.index),
+        fig.add_trace(go.Heatmap(z=[df_pct[col].values], x=df_pct.index, y=[col]*len(df_pct.index),
                                  colorscale='RdYlGn', showscale=True))
-    fig.update_layout(title="Heatmapa Predykcyjna", template="plotly_dark")
+    fig.update_layout(title="Heatmapa Predykcyjna AI", template="plotly_dark")
     return fig
