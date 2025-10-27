322b77b1ae5f55d7c590464db9ac5c6741a302d2
Thorium3324 <118537377+Thorium3324@users.noreply.github.com>
2025-10-27 04:52:51 +0100

    Update strategies.py

--- StockMatrix/tabs/strategies.py
+++ StockMatrix/tabs/strategies.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from utils.strategies import moving_average_strategy
+
+def strategie_tab():
+    st.title("⚙️ Strategie")
+    symbol = st.text_input("Symbol:", "AAPL")
+    short = st.slider("Szybka średnia", 5, 50, 10)
+    long = st.slider("Wolna średnia", 20, 200, 50)
+    if st.button("Uruchom strategię MA"):
+        moving_average_strategy(symbol, short, long)
 
